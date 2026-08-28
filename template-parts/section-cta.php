ce1985bd8f1355e83bb9c3cf32a1c9da6348b33a
mayevskiy <vmayevskiy3009@gmail.com>
2026-08-28 16:01:49 +0300

    feat: cta block

---
+++ template-parts/section-cta.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Часть шаблона: CTA-баннер "Оставьте заявку"
+ */
+?>
+
+<section id="cta" class="section section--cta">
+    <div class="cta">
+
+        <div class="cta__side">
+            <span class="cta__label">Оставьте заявку</span>
+        </div>
+
+        <div class="cta__main">
+            <h2 class="cta__title">Начните инвестировать<br>в реальный актив уже сегодня</h2>
+            <p class="cta__description">Прозрачные условия, короткий цикл, высокий спрос на аренду</p>
+            <a href="#contact" class="cta__button">Оставить заявку</a>
+        </div>
+
+    </div>
+</section>
